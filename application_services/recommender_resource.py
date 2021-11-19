a30c13397e9c7ca4da4613d64b465f3a7d22215a
mmfrenkel <megan.frenkel@gmail.com>
2021-11-19 11:13:08 -0500

    need to add user-based fetch of items

--- application_services/recommender_resource.py
+++ application_services/recommender_resource.py
@@ -1,5 +1,6 @@
 from application_services.base_application_resource import BaseApplicationResource, BaseApplicationException
 import middleware.context as context
+from flask import g
 import asyncio
 import requests
 
@@ -18,12 +19,20 @@ class ArtRecommendationResource(BaseApplicationResource):
         super().__init__()
         self.service_urls = context.get_atomic_microservice_urls()
 
-    @classmethod
-    def get_synchronous_recommendation(cls, limit):
+    def get_synchronous_recommendation(self, limit):
         # get the top popular orders
         # get items already purchased by user
         # recommend any items not already purchased within limit
-        pass
+
+        url = self.service_urls[self.orders_service]
+        headers = {"Authorization": g.access_token}  # use users access token
+        r = requests.get(url=url, headers=headers)
+
+    def get_synchronous_purchase_history(self, user_id):
+        base_url = self.service_urls[self.orders_service]
+        endpoint = ""
+        headers = {"Authorization": g.access_token}  # use users access token
+        return requests.get(url=base_url + endpoint, headers=headers)
 
     @classmethod
     def get_recommendation(cls, limit):
@@ -31,6 +40,6 @@ class ArtRecommendationResource(BaseApplicationResource):
         # get the top popular orders
         # get items already purchased by user
         # recommend any items not already purchased within limit
-        pass
 
+        headers = {"Authorization": g.access_token}  # use users access token
 
