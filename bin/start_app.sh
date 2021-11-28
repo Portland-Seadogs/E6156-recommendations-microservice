bdd63ce3fec0e0a64235936846c3c41e81b5e144
mmfrenkel <megan.frenkel@gmail.com>
2021-11-28 12:34:13 -0500

    oops fixed url

--- bin/start_app.sh
+++ bin/start_app.sh
@@ -2,7 +2,7 @@
 
 # setup environment parameters
 export FLASK_APP='application.py'
-export ORDERS_URL="http://192.168.1.156:5000/"
+export ORDERS_URL="http://192.168.1.156:5000"
 export PRODUCTS_URL="http://192.168.1.156:7777"
 export USERS_ADDRESSES_URL="http://192.168.1.156:8888"
 
