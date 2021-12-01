9aa88a4d869fa5d33b2258ec7c191662e1e62571
mmfrenkel <megan.frenkel@gmail.com>
2021-12-01 16:18:57 -0500

    update urls

--- bin/start_app.sh
+++ bin/start_app.sh
@@ -2,9 +2,9 @@
 
 # setup environment parameters
 export FLASK_APP='application.py'
-export ORDERS_URL="http://192.168.1.156:5000"
-export PRODUCTS_URL="http://192.168.1.156:7777"
-export USERS_ADDRESSES_URL="http://192.168.1.156:8888"
+export ORDERS_URL="http://160.39.244.167:5000"
+export PRODUCTS_URL="http://160.39.244.167:7777"
+export USERS_ADDRESSES_URL="http://160.39.244.167:8888"
 
 # ready to run actual app
 flask run --host=0.0.0.0 --port=9999
